d10841eb0ce49a0e27f8336ebb241b2fe5425e06
PC-EJOUANJAN\ejouanjan <eliott6@live.fr>
2022-12-06 12:37:55 +0100

    App AgeInMinute

---
+++ app/src/main/java/com/eliott/training/ageminute/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.eliott.training.ageminute
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private var tvSelectedDate : TextView? = null
+    private var tvAgeInMinutes : TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
+        btnDatePicker.setOnClickListener{
+            clickDatePicker()
+        }
+
+    }
+
+    private fun clickDatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, selectedYear, selectedMonth, selectedDateOfMonth ->
+                val selectedDate = "$selectedDateOfMonth/${selectedMonth + 1}/$selectedYear"
+                tvSelectedDate?.text = selectedDate
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE)
+                val theDate = sdf.parse(selectedDate)
+                theDate?.let {
+
+                    val selectedDateInMinute = theDate.time / 3600000 // in hours
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    currentDate?.let {
+                        val currentDateInMinute = currentDate.time / 3600000 // in hours
+                        val differenceInMinutes = currentDateInMinute - selectedDateInMinute
+
+                        tvAgeInMinutes?.text = differenceInMinutes.toString()
+                    }
+                }
+            },
+            year,
+            month,
+            day
+
+        )
+        datePickerDialog.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        datePickerDialog.show()
+    }
+}
